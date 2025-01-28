4e2f2b3da26f592126b3a8dbb25b2c37fc35be4b
phnguy2 <phnguy2@ilstu.edu>
2025-01-28 14:40:43 -0600

    Updated code to display temp and humidity on OLED

--- sketch_jan28a/sketch_jan28a.ino
+++ sketch_jan28a/sketch_jan28a.ino
@@ -4,19 +4,29 @@
 
 U8X8_SSD1306_128X64_ALT0_HW_I2C u8x8(/* reset=*/ 
 U8X8_PIN_NONE);
+DHT dht(3, DHT11); //Humididty sensor pin 3
 
 void setup() {
   // put your setup code here, to run once:
   u8x8.begin();
   u8x8.setFlipMode(1);
   u8x8.clear();
+  dht.begin();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  double temp = dht.readTemperature(); //reads temp
+  double humidity = dht.readHumidity(); //reads humidity levels
   u8x8.setFont(u8x8_font_chroma48medium8_r);
   u8x8.setCursor(0, 0);
-  u8x8.print("Hello World!");
+  u8x8.println("Hello World!");
+  u8x8.print("temp: ");
+  u8x8.print(temp);
+  u8x8.println(" F");
+  u8x8.print("humidity: ");
+  u8x8.print(humidity);
+  u8x8.println("%");
 
 }
